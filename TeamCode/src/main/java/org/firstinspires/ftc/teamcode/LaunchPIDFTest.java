fbd3d606afa684dbfa4b234b791edf117df1e253
bwenger-bears <bwenger@uaschools.org>
2026-01-08 12:14:30 -0500

    Clean up - Extra or old files removed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchPIDFTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchPIDFTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.LaunchMotors;
 
+@Disabled
 @TeleOp
 public class LaunchPIDFTest extends OpMode {
     LaunchMotors launchMotor = new LaunchMotors();
